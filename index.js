3bd52918703de565fae1c21df80245894da40b68
brlopez192 <brlopez192@gmail.com>
2025-01-16 16:17:19 -0500

    improvments on API

--- index.js
+++ index.js
@@ -1,68 +1,28 @@
+const { strictEqual } = require('assert');
+const { AsyncResource } = require('async_hooks');
 const express = require('express'), 
     morgan = require('morgan'),
     fs = require('fs'),
+    mongoose = require('mongoose'),
+    Models = require('./models.js'),
     bodyParser = require('body-parser'),
     uuid = require('uuid');
     path = require('path');
 const { title } = require('process');
+const Movies = Models.Movie;
+const Users = Models.User;
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 const accessLogStream = fs.createWriteStream(path.join(__dirname, 'log.txt'), {flags: 'a'})
+mongoose.connect('mongodb://localhost:27017/mymongodb', {useNewUrlParser: true, useUnifiedTopology: true});
 
-let users = [
-    {
-        id: 1,
-        name: "Brandon",
-        favoriteMovies: []
-    },
-    {
-        id: 2,
-        name: "Nicole",
-        favoriteMovies: [
-            "John Wick"
-        ]
-    }
-]
-
-let movies = [
-    {
-        "Title": "Iron Man 2",
-        "Description": "With the world now aware that he is Iron Man, billionaire inventor Tony Stark (Robert Downey Jr.) faces pressure from all sides to share his technology with the military. He is reluctant to divulge the secrets of his armored suit, fearing the information will fall into the wrong hands. With Pepper Potts (Gwyneth Paltrow) and Rhodey (Don Cheadle) by his side, Tony must forge new alliances and confront a powerful new enemy.",
-        "Genre": {
-            "Name": "Sci-fi"
-        },
-        "Director": {
-            "Name": "John Favreau"
-        },
-        "ImageURL": "https://resizing.flixster.com/-XZAfHZM39UwaGJIFWKAE8fS0ak=/v3/t/assets/p3546118_p_v10_af.jpg",
-    },
-    {
-        "Title": "Captain America: The Winter Soldier",
-        "Description": "After the cataclysmic events in New York with his fellow Avengers, Steve Rogers, aka Captain America (Chris Evans), lives in the nation's capital as he tries to adjust to modern times. An attack on a S.H.I.E.L.D. colleague throws Rogers into a web of intrigue that places the whole world at risk. Joining forces with the Black Widow (Scarlett Johansson) and a new ally, the Falcon, Rogers struggles to expose an ever-widening conspiracy, but he and his team soon come up against an unexpected enemy.",
-        "Genre":{
-            "Name": "Adventure"
-        },
-        "Director": {
-            "Name": "Joe & Anthony Russo"
-        },
-        "ImageURL": "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcTfP_GN63NrHEjZ4unvJbGE7neVs5W_6SlUmYHscaXhUSHMkce3eYBL8eiDbwRt4e0oCv0n"
-    },
-    {
-        "Title": "John Wick",
-        "Description": "John Wick is a former hitman grieving the loss of his true love. When his home is broken into, robbed, and his dog killed, he is forced to return to action to exact revenge.",
-        "Genre": {
-            "Name": "Action"
-        },
-        "Director": {
-            "Name": "Chad Stahelski"
-        },
-        "ImageURL": "https://m.media-amazon.com/images/M/MV5BMTU2NjA1ODgzMF5BMl5BanBnXkFtZTgwMTM2MTI4MjE@._V1_.jpg"
-    }
-]
 
 app.use(morgan('combines', {stream: accessLogStream}));
 app.use(express.static('pubblic/documentation.html'));
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', (req, res) => {
     res.send('Weolcome to My Flix app!');
@@ -72,33 +32,155 @@ app.get('/documentation', (req, res) => {
     res.sendFile('public/documentation.html', {root: __dirname});
 });
 
-// Create new User
-app.post('/users', (req, res) => {
-    const newUser = req.body;
 
-    if (newUser.name) {
-        newUser.id = uuid.v4();
-        users.push(newUser);
-        res.status(201).json(newUser);
-    } else {
-        res.status(400).send("Users Need Names");
-    }
-})
+// Get all Movies
+app.get('/movies', async (req, res) => {
+    await Movies.find()
+    .then((movies) => {
+        res.status(201).json(movies);
+    })
+    .catch((err) => {
+        console.error(err);
+        res.status(500).send('Error:' + err);
+    });
+});
 
-// Allow user to Update User Info
-app.put('/users/:id', (req, res) => {
-    const { id } = req.params;
-    const updatedUser = req.body;
+// Return movie data for one movie
+app.get('/movies/:Title', async (req, res) => {
+    await Movies.findOne({Title: req.params.Title })
+    .then((movie) => {
+        res.json(movie);
+    })
+    .catch((err) => {
+        console.error(err);
+        res.status(500).send('Error: ' + err);
+    });
+});
 
-    let user = users.find(user => user.id == id);
+// Return data about a director
+app.get('/movies/:Director', async (req, res) => {
+    await Movies.findOne({Director: req.params.Director })
+    .then((movie) => {
+        res.json(Director);
+    })
+    .catch((err) => {
+        console.error(err);
+        res.status(500).send('Error: ' + err);
+    });
+});
 
-    if (user) {
-        user.name = updatedUser.name;
-        res.status(200).json(user);
-    } else {
-        res.status(400).send("No Such User");
-    }
-})
+// Add a user
+app.post('/users', async (req, res) => {
+    await Users.findOne({ Username: req.body.Username })
+    .then((user) => {
+        if (user) {
+            return res.status(400).send(reqbody.Username + 'already exists');
+        } else {
+            Users
+            .create ({
+                Username: req.body.Username,
+                Password: req.boddy.Password,
+                Email: req.body.Email,
+                Birthday: req.body.Birthday
+            })
+            .then((user) =>{res.status(201).removeAllListeners(user) })
+            .catch((error) => {
+                console.error(error);
+                res.status(500).send('Error: ' + error);
+            })
+        }
+    })
+    .catch((error) => {
+        console.error(error);
+        res.status(500).send('Error: ' + error);
+    });
+});
+
+// Get all users
+app.get('/users', async (req, res) => {
+    await Users.find()
+    .then((users) => {
+        res.status(201).json(users);
+    })
+    .catch((err) => {
+        console.error(err);
+        res.status(500).send('Error:' + err);
+    });
+});
+
+// Get a user by username
+app.get('/users/:Username', async (req, res) => {
+    await Users.findOne({Username: req.params.Username })
+    .then((user) => {
+        res.json(user);
+    })
+    .catch((err) => {
+        console.error(err);
+        res.status(500).send('Error: ' + err);
+    });
+});
+
+
+// Update a user's info by Username
+/*
+Required Elements:
+Usersname: String
+Password: String
+Email: String
+
+Not Required:
+Birthday: Date
+*/
+
+app.put('/users/:Username', async (req, res) => {
+    await Users.findOneAndUpdate({Username: req.params.Username }, {$set: 
+        {
+            Username: req.body.Username,
+            Password: req.body.Password,
+            Email: req.body.Email,
+            Birthday: req.body.Birthday
+        }
+    }, 
+    {new: true }) // This line makes sure that the updated document is returned
+    .then((updatedUser) => {
+        res.json(updatedUser);
+    })
+    .catch((err) => {
+        console.error(err);
+        res.status(500).send('Error:' + err);
+    });
+});
+
+// Delete a user by username
+app.delete('/users/:Username', async (req, res) => {
+    await Users.findOneAndRemove({Username: req.params.Username})
+    .then((user) => {
+        if (!user) {
+            res.status(400).send(req.params.Username + 'was not found');
+        } else {
+            res.status(200).send(req.params.Username + 'was deleted.');
+        }
+    })
+    .catch((err) => {
+        console.error(err);
+        res.status(500).send('Error: ' + err);
+    });
+});
+
+// Add a movie to a user's list of favorites
+app.post('/users/:Username/movies/:MovieID', async (req, res) => {
+    await Users.findOneAndUpdate({Username: req.params.Username}, {
+        $push: {favoriteMovies: req.params.MovieID}
+    },
+    {new: true}) // This line makes sure that the updated document is returned
+    .then((updatedUser) => {
+        res.json(updatedUser);
+    })
+    .catch((err) => {
+        console.error(err);
+        res.status(500).send('Error: ' + err);
+    });
+});
 
 // Allow users to add movies to their list (only return a movie has been added)
 app.post('/users/:id/:movieTitle', (req, res) => {
@@ -114,76 +196,7 @@ app.post('/users/:id/:movieTitle', (req, res) => {
     }
 })
 
-// Allow users to remove a movie from their favorites
-app.delete('/users/:id/:movieTitle', (req, res) => {
-    const { id, movieTitle } = req.params;
 
-    let user = users.find(user => user.id == id);
-
-    if (user) {
-        user.favoriteMovies = user.favoriteMovies.filter(title => title !== movieTitle);
-        res.status(200).send(`${movieTitle} has been removed from user ${id}'s array`);
-    } else {
-        res.status(400).send("No Such User");
-    }
-});
-
-// Allow a user to deregister
-app.delete('/users/:id', (req, res) => {
-    const { id } = req.params;
-
-    let user = users.find(user => user.id == id);
-
-    if (user) {
-        users = users.filter(user => user.id != id);
-        res.status(200).send(`user ${id} has been deleted.`);
-    } else {
-        res.status(400).send("No Such User");
-    }
-});
-
-
-
-// Read
-app.get('/movies', (req, res) => {
-    res.status(200).json(movies);
-});
-
-// Read
-app.get('/movies/:title', (req, res) => {
-    const { title } = req.params;
-    const movie = movies.find(movie => movie.Title === title);
-
-    if (movie) {
-        res.status(200).json(movie);
-    } else {
-        res.status(400).send('No Such Movie');
-    }
-});
-
-// Read
-app.get('/movies/genre/:genreName', (req, res) => {
-    const { genreName } = req.params;
-    const genre = movies.find(movie => movie.Genre.Name === genreName).Genre;
-
-    if (genre) {
-        res.status(200).json(genre);
-    } else {
-        res.status(400).send('No Such Genre');
-    }
-});
-
-// Read
-app.get('/movies/director/:directorName', (req, res) => {
-    const { directorName } = req.params;
-    const director = movies.find(movie => movie.Director.Name === directorName).Director;
-
-    if (director) {
-        res.status(200).json(director);
-    } else {
-        res.status(400).send('No Such Director');
-    }
-});
 
 app.use((err, req, res, next) => {
     console.error(err.stack);
