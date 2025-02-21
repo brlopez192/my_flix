f98dc30bd05176d482fc5deada95501540af15c8
brlopez192 <brlopez192@gmail.com>
2025-02-21 04:01:05 -0500

    code fixes

--- index.js
+++ index.js
@@ -116,7 +116,7 @@ app.post('/users',
                 Email: req.body.Email,
                 Birthday: req.body.Birthday
             })
-            .then((user) =>{res.status(201).removeAllListeners(user) })
+            .then((user) =>{res.status(201).json(user) })
             .catch((error) => {
                 console.error(error);
                 res.status(500).send('Error: ' + error);
