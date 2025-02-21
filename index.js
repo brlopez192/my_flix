0feb66afd22ce47e0ab6b5ff58e03022ae69ddc8
brlopez192 <brlopez192@gmail.com>
2025-02-21 04:16:33 -0500

    app updates

--- index.js
+++ index.js
@@ -26,18 +26,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 const cors = require('cors');
-let allowedOrigins = ['http://localhost:8080', 'http://testsite.com', 'https://theflixer.netlify.app/'];
+let allowedOrigins = ['http://localhost:1234', 'http://testsite.com', 'https://theflixer.netlify.app/'];
 
-app.use(cors({
-  origin: (origin, callback) => {
-    if(!origin) return callback(null, true);
-    if(allowedOrigins.indexOf(origin) === -1){
-      let message = "The CORS policy for this application doesn't allow access from origin " + origin;
-      return callback(new Error(message ), false);
-    }
-    return callback(null, true);
-  }
-}));
+app.use(cors())
 
 let auth = require('./auth')(app);
 const passport = require('passport');
